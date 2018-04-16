444a2736e5e90e1b1c6677b75e22809c237f3182
Rangel Reale <rangelspam@gmail.com>
2018-04-16 14:21:48 -0300

    Can generate single file

--- fdepgen.go
+++ fdepgen.go
@@ -22,26 +22,38 @@ func (g *FDepGen) Generate(output FileOutput) error {
 
 	for _, df := range g.Dep.Files {
 		if df.DepType == fdep.DepType_Own {
-			g, err := g.CreateGeneratorFunc(g.Dep, df)
+			err := g.GenerateFile(df, output)
 			if err != nil {
 				return err
 			}
+		}
+	}
 
-			err = g.Generate()
+	return nil
+}
+
+// Output must have been called initialize before calling this
+func (g *FDepGen) GenerateFile(depfile *fdep.DepFile, output FileOutput) error {
+	output.Initialize()
+	defer output.Finalize()
+
+	gn, err := g.CreateGeneratorFunc(g.Dep, depfile)
+	if err != nil {
+		return err
+	}
+
+	err = gn.Generate()
+	if err != nil {
+		return err
+	}
+
+	// write all files
+	for _, gf := range gn.Files() {
+		if gf != nil && gf.HaveData() {
+			err = output.Output(gf)
 			if err != nil {
 				return err
 			}
-
-			// write all files
-			for _, gf := range g.Files() {
-				if gf != nil && gf.HaveData() {
-					err = output.Output(gf)
-					if err != nil {
-						return err
-					}
-				}
-			}
-
 		}
 	}
 
